27a4de993c7fac7a618328998aaa09e9316a6266
Luciano Larrea <takticflow@gmail.com>
2025-03-06 08:49:38 -0300

    Add switch language and Botpress.js

---
+++ botpress.js
@@ -0,0 +1,11 @@
+// botpress.js
+(function() {
+    //Botpress Webchat Scripts
+    const script1 = document.createElement('script');
+    script1.src = 'https://cdn.botpress.cloud/webchat/v2.2/inject.js';
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.src = 'https://files.bpcontent.cloud/2025/03/05/13/20250305130950-I024HGGW.js';
+    document.head.appendChild(script2);
+})();
